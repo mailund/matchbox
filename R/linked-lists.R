c31c160ab371e617b63d74af635db45b70bcc4f9
Thomas Mailund <mailund@birc.au.dk>
2018-03-07 20:57:50 +0100

    Some linked list functions
    I am trying to figure out how to get this through the package check, but the NSE in pmatch causes some problems as does the transformations from tailr.

--- R/linked-lists.R
+++ R/linked-lists.R
@@ -1,5 +1,38 @@
+
+# dummy variables for documentation
+
+#' An empty list
+#' @export
+NIL <- NULL
+#' Constructs a new linked list from an element and another list
+#' @param car  The head of the new list
+#' @param cdr  The remainder of the list
+#' @return A new list
+#' @export
+CONS <- function(car, cdr) {}
+#' Print linked list objects
+#' @param x   The linked list
+#' @param ... Additional parameters (not used in this version)
+#' @export
+print.llist <- function(x, ...) {}
+#' Make a string-representation of a linked list.
+#' @param x The linked list
+#' @param ... Additional parameters (not used in this version)
+#' @return A string representation of the list
+#' @export
+toString.llist <- function(x, ...) {}
+
 pmatch::`:=`(llist, NIL | CONS(car, cdr : llist))
 
+#' Compute the length of a linked list.
+#'
+#' This function computes the length of a linked list by scanning
+#' through it. This is thus a linear time function.
+#'
+#' @param llist The linked list
+#' @param acc   An accumulator (for making the function tail-recursive)
+#' @return The length of `llist`
+#'
 #' @export
 llength <- function(llist, acc = 0) {
     pmatch::cases(
@@ -10,6 +43,14 @@ llength <- function(llist, acc = 0) {
 }
 llength <- tailr::loop_transform(llength)
 
+#' Reverse a linked list.
+#'
+#' This function scans through `llist`, building the reverse list in `acc`.
+#'
+#' @param llist A linked list
+#' @param acc   An accumulator (for making the function tail-recursive)
+#' @return A list containing the elements in `llist` in reverse order.
+#'
 #' @export
 llrev <- function(llist, acc = NIL) {
     pmatch::cases(
@@ -21,6 +62,11 @@ llrev <- function(llist, acc = NIL) {
 llrev <- tailr::loop_transform(llrev)
 
 
+#' Translate a list object into a linked list.
+#'
+#' @param x A `list` object
+#' @return A linked list containing the elements in `x`
+#'
 #' @export
 llist_from_list <- function(x) {
     llist <- NIL
--- tests/testthat/test-linked-lists.R
+++ tests/testthat/test-linked-lists.R
@@ -1,7 +1,17 @@
 context("test-linked-lists.R")
 
 test_that("we can construct linked lists", {
-    #lst <- NIL
+    lst <- NIL
+    expect_equal(
+        pmatch::cases(lst, NIL -> 1),
+        1
+    )
 
-    #expect_equal(cases(lst, NIL -> 1), 1)
+    lst <- CONS(2, NIL)
+    expect_equal(
+        pmatch::cases(lst,
+                       NIL -> 1,
+                       CONS(elm, NIL) -> elm),
+        2
+    )
 })
